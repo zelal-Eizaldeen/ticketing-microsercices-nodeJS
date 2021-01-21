e1120d0e4d66b5d4f9f7d408b6e8a6fbedc9dd05
ayman-zouby <engaymanz@gmail.com>
2021-01-21 14:18:59 +0300

    added 249 session right

--- Ticketing/tickets/src/app.ts
+++ Ticketing/tickets/src/app.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@sgtickets/common';
-
+import { errorHandler, NotFoundError } from '@zetickets/common';
+import { createTicketRouter } from './routes/new';
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -13,7 +13,7 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-
+app.use(createTicketRouter);
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
